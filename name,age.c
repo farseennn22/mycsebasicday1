da45900a73618e851e876f72682a7eea32bb9490
farseennn22 <afrafarseen@gmail.com>
2025-05-13 11:58:51 +0530

    Create name,age.c

---
+++ name,age.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char name[40];
+    int age;
+    printf("Enter your name:");
+    scanf("49s", name);
+    printf("Enter your age:");
+    scanf("%d",&age);
+    printf("Hello %s, you are %d years old.\n",name,age);
+    return 0;
+}
+Enter your name:farseen
+Enter your age:Hello , you are 0 years old.
+
+
+=== Code Execution Successful ===
