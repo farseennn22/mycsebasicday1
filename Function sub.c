c8f4f5eb793c23c69d9a7d6fd649417b2e1da741
farseennn22 <afrafarseen@gmail.com>
2025-05-13 15:26:56 +0530

    Create Function sub.c

---
+++ Function sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sub(int a,int b)
+{
+    int c=a-b;
+    return c;
+}
+int main()
+{
+int result=sub(4,2);
+printf("Result is%d", result);
+    return 0;
+}
+Result is2
+
+=== Code Execution Successful ===
