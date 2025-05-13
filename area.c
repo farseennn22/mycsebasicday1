3e428c2e7287b75884c694003b71c8520b6d0970
farseennn22 <afrafarseen@gmail.com>
2025-05-13 14:28:57 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    float length,width,area;
+    printf("Enter the length of the rectangle:");
+    scanf("%f",&length);
+    printf("Enter the width of the rectangle:");
+    scanf("%f",&width);
+    area=length*width;
+    printf("the area of the rectangle:%.2f\n",area);
+    return 0;
+}
+Enter the length of the rectangle:6
+Enter the width of the rectangle:4
+the area of the rectangle:24.00
+
+
+=== Code Execution Successful ===
