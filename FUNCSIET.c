e5236acf5b18d84d6723f49b95648cd888a8827d
farseennn22 <afrafarseen@gmail.com>
2025-05-13 15:58:16 +0530

    Create FUNCSIET.c

---
+++ FUNCSIET.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int shridevi()
+{
+  printf("farseen\n");
+    printf("date of birth\n");
+    printf("age\n");
+    printf("mobilenum\n");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
