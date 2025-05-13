668c71c7b3e883dfe3c705b8e1c9f92864a57648
farseennn22 <afrafarseen@gmail.com>
2025-05-13 11:04:56 +0530

    Create af.c

---
+++ af.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the number:");
+    scanf("%d",&num);
+    if(num>0)
+    {
+        printf("the enered number is positive");
+    }
+    return 0;
+}
+enter the number:64
+the enered number is positive
+
+=== Code Execution Successful ===
