4fa62c93f946d33a07291018e8f5b072d234bf7a
farseennn22 <afrafarseen@gmail.com>
2025-05-13 13:58:47 +0530

    Create name,age,location.c

---
+++ name,age,location.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    printf("Name:farseen\n");
+    printf("Age: 23\n");
+    printf("Location:Tumkur\n");
+    return 0;
+}
+Name:farseen
+Age: 23
+Location:Tumkur
+
+
+=== Code Execution Successful ===
