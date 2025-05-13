45b54f2c03a82d0f128e81107d0f0a814db5a37b
farseennn22 <afrafarseen@gmail.com>
2025-05-13 14:20:03 +0530

    Create ASCII.c

---
+++ ASCII.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    printf("THE ASCII VALUE of '%c' is %d\n",ch,ch);
+    return 0;
+}
+Enter a character:M
+THE ASCII VALUE of 'M' is 77
+
+
+=== Code Execution Successful ===
