49485193a2d594b58972062ace199426841bdd99
farseennn22 <afrafarseen@gmail.com>
2025-05-14 21:08:09 +0530

    Create sq of no.c

---
+++ sq of no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int sq(int n){
+    return n*n;
+}
+int main(){
+    int n=5;
+    int result=sq(n);
+    printf("The square of %d\n",n,result);
+}
+The square of 5
+
+
+=== Code Execution Successful ===
