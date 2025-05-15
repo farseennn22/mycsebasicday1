39b09ca0eec94cc519ebc873430fecc472114bd9
farseennn22 <afrafarseen@gmail.com>
2025-05-15 20:52:16 +0530

    Create declaring int.c

---
+++ declaring int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=4;
+    int *ptr=&a;
+    printf("a is:%d\n",ptr);
+    printf("address is:%d\n",ptr);
+    return 0;
+}
+a is:-1285074188
+address is:-1285074188
+
+
+=== Code Execution Successful ===
