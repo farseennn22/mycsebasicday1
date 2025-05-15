d0c0b8929b4126a03bef0f22895515f0c52b9659
farseennn22 <afrafarseen@gmail.com>
2025-05-15 12:47:20 +0530

    Create call by value.c

---
+++ call by value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    int b=a;
+    b=b+10;
+    printf("%d\n",a);
+    printf("%d\n",b);
+    return 0;
+}
+5
+15
+
+
+=== Code Execution Successful ===
