3e1155ff60e0ae850c50335b33dae5a46910c6c6
farseennn22 <afrafarseen@gmail.com>
2025-05-15 09:48:15 +0530

    Create elements.c

---
+++ elements.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int numbers[5]={10,20,30,40,50};
+    printf("first element: %d\n",numbers[0]);
+    printf("third elemen: %d\n", numbers[2]);
+    return 0;
+}
+first element: 10
+third elemen: 30
+
+
+=== Code Execution Successful ===
