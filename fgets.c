9f4ff423b38975360c07d3b5eaa764fba13125bd
farseennn22 <afrafarseen@gmail.com>
2025-05-15 21:05:23 +0530

    Create fgets.c

---
+++ fgets.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char name[10];
+    printf("Enter your name:");
+    fgets(name,10,stdin);
+    printf("your name is:%s",name);
+    return 0;
+}
+Enter your name:A h a m e d
+your name is:A h a m e
+
+=== Code Execution Successful ===
