a966474a9ab3203c681617e4b8bb97d97eda7992
farseennn22 <afrafarseen@gmail.com>
2025-05-16 12:13:26 +0530

    Create array of pointer.c

---
+++ array of pointer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int arr[10]={1,2,3,4},n=4,i,temp,*start,*end;
+    start=arr;// first index start=0
+    end=arr+n-1;//last index ens=3
+    while(start<end){
+        temp=*start;
+        *start=*end;
+        *end=temp;
+        start++;
+        end--;
+        for(i=0;i<n;i++){
+            printf("%d",arr[i]);
+        }
+    }
+}
+42314321
+
+=== Code Execution Successful ===
