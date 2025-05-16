fd32f8e43926e02966491cb58d0c6389ced88c5c
farseennn22 <afrafarseen@gmail.com>
2025-05-16 11:02:31 +0530

    Create prime number.c

---
+++ prime number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int i,num,isprime=1;
+int main(){
+    printf("Enter the num starting from positive 2");
+    scanf("%d",&num);
+    for(i=2;i<=num/2;i++){
+        if(nEnter the num starting from positive 2
+5
+it a prime
+
+
+=== Code Execution Successful ===um%2==0){
+            printf("its not a prime\n");
+        }
+        else{
+        printf("it a prime\n");
+    }
+    }
+} 
+
+
